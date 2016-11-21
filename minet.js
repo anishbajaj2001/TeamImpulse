98a4292314d539fb948181f80dffded2126e153a
anishbajaj2001 <anishbajaj01@gmail.com>
2016-11-21 15:08:06 +0530

    Add files via upload

---
+++ minet.js
@@ -0,0 +1,8 @@
+/*window.onload = function() {
+	document.body.style.opacity = "1";
+	for (i = 0; i < document.querySelectorAll("a").length; i++) {
+		document.querySelectorAll("a")[i].addEventListener("click", function() {
+			document.body.style.opacity = "0";
+		});
+	}
+}*/
